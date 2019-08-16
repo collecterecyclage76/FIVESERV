c9ae704533be8d1159f0236a26e2ea9d963d9976
collecterecyclage76 <51483131+collecterecyclage76@users.noreply.github.com>
2019-08-16 21:42:49 +0200

    base serveur
    ...

---
+++ FIVE/resources/[system]/baseevents/server.lua
@@ -0,0 +1,33 @@
+RegisterServerEvent('baseevents:onPlayerDied')
+RegisterServerEvent('baseevents:onPlayerKilled')
+RegisterServerEvent('baseevents:onPlayerWasted')
+RegisterServerEvent('baseevents:enteringVehicle')
+RegisterServerEvent('baseevents:enteringAborted')
+RegisterServerEvent('baseevents:enteredVehicle')
+RegisterServerEvent('baseevents:leftVehicle')
+
+AddEventHandler('baseevents:onPlayerKilled', function(killedBy, data)
+	local victim = source
+
+	RconLog({msgType = 'playerKilled', victim = victim, attacker = killedBy, data = data})
+end)
+
+AddEventHandler('baseevents:onPlayerDied', function(killedBy, pos)
+	local victim = source
+
+	RconLog({msgType = 'playerDied', victim = victim, attackerType = killedBy, pos = pos})
+end)
+
+local name    = 'hunger'
+local default = 1000000
+local color   = '#CFAD0F'
+
+TriggerEvent('esx_status:registerStatus', name, default, color, 
+	function(status) -- Visible calllback, if it return true the status will be visible
+		return true
+	end,
+	function(status) -- Tick callback, what to do at each tick
+		status.remove(200)
+	end,
+	{remove = 200} -- Client action (add / remove) so the client can be in sync with server
+)
---
+++ FIVE/starter.bat
@@ -0,0 +1,2 @@
+cd /C C:\gta\cfx-server-data
+C:\gta\cfx-server-data\run.cmd +exec server.cfg
